a50068c081ed41f8b8368218ea2149af98583a27
Kcepriu <kcepriu@gmail.com>
2023-01-28 15:56:33 +0200

    First commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,10 @@
 import { Component } from 'react';
-import Section from './Section';
-import FeedbackOptions from './FeedbackOptions';
-import Statistics from './Statistics';
 
 class App extends Component {
-  buttonsFeedback = ['good', 'neutral', 'bad'];
-
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
-  hendlerClick = typeButtom => {
-    this.setState(prevState => ({ [typeButtom]: prevState[typeButtom] + 1 }));
-  };
-
   render() {
     return (
       <div className="App">
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={this.buttonsFeedback}
-            onLeaveFeedback={this.hendlerClick}
-          />
-        </Section>
-
-        <Section title="Statistics">
-          <Statistics
-            goodValue={this.state.good}
-            neutralValue={this.state.neutral}
-            badValue={this.state.bad}
-          />
-        </Section>
+        <p>Hello</p>
       </div>
     );
   }
--- src/components/Notification/Notification.styled.js
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const TextNotification = styled.p`
-  font-size: 20px;
-`;
